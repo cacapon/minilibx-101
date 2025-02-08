b504b0e80bdb6411932269b44bb4b00172b40000
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-02-08 12:21:29 +0900

    02 created

--- 02-draw-map/main.c
+++ 02-draw-map/main.c
@@ -6,20 +6,32 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 12:54:51 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/02/06 16:35:06 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/02/08 12:17:44 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
-#include <math.h>
 
-static void scale(void *mlx, t_xpm_img *img, int scale)
+static t_xpm_img _load_xpm(void *mlx)
 {
-	int bits_per_pixel;
-	int size_line;
-	int endian;
-	char *data;
-	
+	t_xpm_img	xpm;
+
+	xpm = (t_xpm_img){.h=16, .w=16, .h_p=&(xpm.h), .w_p=&(xpm.w)};
+	xpm.img[0] = mlx_xpm_file_to_image(mlx, "02-draw-map/plane.xpm", xpm.w_p, xpm.h_p);
+	xpm.img[1] = mlx_xpm_file_to_image(mlx, "02-draw-map/wall.xpm", xpm.w_p, xpm.h_p);
+	return (xpm);
+}
+
+static void _set_stage(void *mlx, void *win, t_xpm_img xpm)
+{
+	for (int y = 0; y<5; y++)
+		for (int x = 0; x<5; x++)
+		{
+			if (x == 0 || y == 0 || x == 4 || y == 4)
+				mlx_put_image_to_window(mlx, win, xpm.img[1], xpm.w*x, xpm.h*y);
+			else
+				mlx_put_image_to_window(mlx, win, xpm.img[0], xpm.w*x, xpm.h*y);
+		}
 }
 
 int	main(void)
@@ -27,23 +39,11 @@ int	main(void)
 	void		*mlx;
 	void		*win;
 	t_xpm_img	xpm;
-	int			xpm_h;
-	int			xpm_w;
 
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, 500, 500, "mlx-101-01 show window");
-	xpm_h = 16;
-	xpm_w = 16;
-	xpm.img[0] = mlx_xpm_file_to_image(mlx, "02-draw-map/plane.xpm", &xpm_w, &xpm_h);
-	xpm.img[1] = mlx_xpm_file_to_image(mlx, "02-draw-map/wall.xpm", &xpm_w, &xpm_h);
-	for (int y = 0; y<5; y++)
-		for (int x = 0; x<5; x++)
-		{
-			if (x == 0 || y == 0 || x == 4 || y == 4)
-				mlx_put_image_to_window(mlx, win, xpm.img[1], xpm_w*x, xpm_h*y);
-			else
-				mlx_put_image_to_window(mlx, win, xpm.img[0], xpm_w*x, xpm_h*y);
-		}
+	xpm = _load_xpm(mlx);	
+	_set_stage(mlx, win, xpm);
 	mlx_loop(mlx);
 	return (0);
 }
--- 02-draw-map/main.h
+++ 02-draw-map/main.h
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 15:28:28 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/02/06 16:17:05 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/02/08 11:42:24 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@
 typedef struct e_xpm_img
 {
 	void	*img[2];
-	int		*width;
-	int		*height;
+	int		w;
+	int		h;
+	int		*w_p;
+	int		*h_p;
 }			t_xpm_img;
 
 #endif
